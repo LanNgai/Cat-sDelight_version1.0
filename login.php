0951897ae45bf417fa226f4542b7e2d203242a8a
Lan Ngai <b00159127@mytudublin.ie>
2025-02-23 21:21:34 +0000

    initial

---
+++ login.php
@@ -0,0 +1,19 @@
+
+<html>
+<body>
+<h3>Login</h3>
+<form method="post" action="account.php">
+    Your name:
+    <br>
+    <input type="text" name="username">
+    <br><br>
+    Your password:
+    <br>
+    <input type="text" name="password">
+    <br><br>
+    <input type="submit" value="submit login details">
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
+
